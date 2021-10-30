972581c905057992a01d57200e56673df5b66e7e
lcnde <176673026+lcnde@users.noreply.github.com>
2021-10-30 05:59:59 +0200

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React, {useState, useEffect} from 'react';
 import './styles/App.scss';
+import '../src/styles/cssReset.scss';
+import Cards from './components/Cards.js';
 
 function App() {
   return (
     <div className="App">
-      
+      <Cards />
     </div>
   );
 }
